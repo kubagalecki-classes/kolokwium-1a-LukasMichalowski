a0b281601da3cae8f565df829d421879d95f21a6
LukasMichalowsk <lukpio97@gmail.com>
2021-01-14 11:34:27 +0000

    test 1

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -6,3 +6,19 @@
 
 // Zad1
 // tutaj definicja klasy Tagliatelle
+class Tagliatelle{
+private:
+double L;
+double W;
+double R;
+const double C;
+public:
+    Tagliatelle(): L(0.5), W(0.5), R(0.5), C(0.5) {};
+    Tagliatelle(double l,double w,double r,double c):  L(l), W(w), R(r), C(c) {};
+
+    double ileMaki(unsigned P) const {
+        return  P*L*W*(1.-R)*C;
+    }
+
+
+};
